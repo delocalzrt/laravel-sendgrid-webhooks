0116bbedea3250e9e0f4e4b30781aafb89035d97
stellar124 <stellar124@gmail.com>
2023-07-19 09:06:49 +0200

    Update 2019_05_22_000000_update_category_column_to_array.php

--- database/migrations/2019_05_22_000000_update_category_column_to_array.php
+++ database/migrations/2019_05_22_000000_update_category_column_to_array.php
@@ -13,47 +13,10 @@ class UpdateCategoryColumnToArray extends Migration
      */
     public function up()
     {
-
-
-        $connection = config('database.default');
-        $driver = config("database.connections.{$connection}.driver");
-
-        // Move old `category` values over to new `categories` json array
-        switch ($driver) {
-            case 'sqlite': {
-                Schema::table('sendgrid_webhook_events', function (Blueprint $table) {
-                    $table->jsonb('categories')->default(json_encode([]))->index();
-                });
-
-                DB::table('sendgrid_webhook_events')
-                    ->whereNotNull('category')
-                    ->update(['categories' => DB::raw("CAST('[\"' || category || '\"]' AS json)")]);
-                break;
-            }
-
-            case 'mysql': {
-                Schema::table('sendgrid_webhook_events', function (Blueprint $table) {
-                    $table->jsonb('categories')->default(json_encode([]));
-                    $table->index([DB::raw('categories(767)')], 'categories_index');
-                });
-
-                DB::table('sendgrid_webhook_events')
-                    ->whereNotNull('category')
-                    ->update(['categories' => DB::raw("concat('{\"', category, '\"}')")]);
-                break;
-            }
-
-            default: {
-                Schema::table('sendgrid_webhook_events', function (Blueprint $table) {
-                    $table->jsonb('categories')->default(json_encode([]))->index();
-                });
-
-                DB::table('sendgrid_webhook_events')
-                    ->whereNotNull('category')
-                    ->update(['categories' => DB::raw("CAST(concat('[\"', category, '\"]') AS json)")]);
-                break;
-            }
-        }
+          Schema::table('sendgrid_webhook_events', function (Blueprint $table) {
+                $table->jsonb('categories')->nullable();
+                $table->index([DB::raw('categories(767)')], 'categories_index');
+            });
     }
 
     /**
